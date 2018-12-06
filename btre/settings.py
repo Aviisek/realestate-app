a10461d494103c796c81aea8aa6813ef377529f9
Abhishek <abhishekjec@hotmail.com>
2018-12-06 20:02:38 +0530

    added changes for heroku

--- btre/settings.py
+++ btre/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+#Added for heruko deployment
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +146,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
